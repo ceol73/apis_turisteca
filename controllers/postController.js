8a14408e8415e982b86c7bfb1a2f8539f7c47220
ceol73 <cedillo.olazaran.21070@itsmante.edu.mx>
2025-04-03 11:10:16 -0600

    Eliminar la extensión .js de la ruta de importación en postController

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,4 +1,4 @@
-const PostRepository = require('../repositories/PostRepository.js'); // Added .js extension
+const PostRepository = require('../repositories/PostRepository'); // Removed .js extension
 const { body, validationResult } = require('express-validator');
 const APIresponse = require('../middlewares/APIresponse');
 
